764d8c5f309eec5b435c51aaa6befdd5c5a07130
Artlant <xufo@ukr.net>
2017-02-19 16:40:37 +0200

    fix

--- README.md
+++ README.md
@@ -6,19 +6,24 @@ Opportunities and script logic:
 - The conclusion of the system message
 - And the opening new mail/s in a browser
 
-### Requirements 
+### Requirements
 > - python => 2.0
   - configparser
-  - urllib
   - feedparser
- 
+  - urllib
+  - furl
+
+```
+sudo pip install configparser feedparser furl
+```
+
 ### Installation Instructions
 1. Copy file [gmail.py](/gmail.py) OR repo `$ git clone https://github.com/Artlant/gmail-check.git`
 2. Make sure the script is executable by running `chmod +x ./gmail.py`
 3. Run `$ ./gmail.py`
 4. Change in `gmail_settings.cfg` you `login` and `pass` from gmail account
-5. Change in file `gmail.py` 103 line "__/home/user/.local/gmail_settings.cfg__" from you path conf
-
+5. Change in file `gmail.py` variable `configFilePath` from you path conf
+6. (optional) Change variable `DEBUG_MODE` = `True`/`False`  in `gmail.py`
 
 __//period call script should be implemented you__. For example in linux:
 ```
@@ -26,5 +31,5 @@ watch -n 300./gmail.py
 ```
 where `-n sec` - every sec-seconds
 
-### License 
+### License
 [![MIT License](https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square)](/LICENSE)
--- gmail.py
+++ gmail.py
@@ -1,22 +1,116 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+# import local module
+# import pkgutil
+# import importlib
+# packages = pkgutil.walk_packages(path='./lib/')
+# packages = pkgutil.walk_packages(path='.')
+# for importer, name, is_package in packages:
+#     mod = importlib.import_module(name)
+
+DEBUG_MODE = True
+# DEBUG_MODE = False
+configFilePath = ('/home/user/.local/gmail_settings.cfg')
+
+
+import sys
+import os
+import time
+import configparser
+import feedparser
+import webbrowser
+from furl import furl
+from urllib.request import FancyURLopener
+
+
+module_list = [
+    "sys",
+    "os",
+    "time",
+    "configparser",
+    "feedparser",
+    "webbrowser",
+    "furl",
+    "urllib"
+]
+
+# module_list = {
+#     'import': 'time',
+#     'import': 'configparser',
+#     'import': 'feedparser',
+#     'import': 'webbrowser',
+#     'import': 'furl'
+# }
+
+# check in file project
+def module_exists(module_name):
+    # import sys
+    if not module_name in sys.modules.keys():
+        print("ImportError: " + module_name)
+        # import pip
+        # pip.main(['install', module_name])
+    else:
+        debug_echo("%s -> %s" % ("import", module_name))
+
+
+# check in system
+# NOW OFF
+def module_exists_OFF(module_name):
+    try:
+        __import__(module_name)
+        # import module_name
+    except ImportError:
+        # print("ImportError: " + module_name)
+        # raise ImportError("ImportError:", module_name)
+        return False
+        # import pip
+        # pip.main(['install', module_name])
+    else:
+        debug_echo("%s -> %s" % ("import", module_name))
+        return True
+
+
+def module_import(module_list):
+    for item in module_list:
+        # debug_echo("%s -> %s" % ("import", item))
+        module_exists( item )
+        # print ( "%s -> %s" % (item, module_list[item]) )
+        # module_exists( module_list[item] )
+
+
+
+
+def debug_echo(msg):
+    if DEBUG_MODE == True:
+        print('>> ' + msg)
+        # log.debug('>>', exc_info=True)
+    else:
+        return False
+
 
 def getConfig(configFilePath):
-    import configparser
+    # import configparser
+
+    debug_echo('reading config file:' + configFilePath)
+
+    # # if not configFilePath:
+    # if configFilePath in locals():
+    #     print('config file not found')
 
     config = configparser.ConfigParser()
-    # configFilePath = ''
     config.read(configFilePath)
     try:
-        USER = config.get('config', 'user') # @gmail.com can be left out
+        USER = config.get('config', 'user')  # @gmail.com can be left out
         PASSWD = config.get('config', 'passwd')
 
         url = getMailUrl(USER, PASSWD)
         return url
 
     except Exception as e:
-        print(str(e), ' could not read configuration file')
-        import sys
-        sys.exit()
+        print(str(e), ' could not read config file')
+        # import sys
+        # sys.exit()
+        raise
 
 
 def getMailUrl(user, pwd):
@@ -24,29 +118,42 @@ def getMailUrl(user, pwd):
 
 
 def sendNoti(head, msg):
-    import os
+    # import os
     return os.system('notify-send "' + head + '" "' + msg + '"')
     # os.system('notify-send "Gmail" "new '+fullcount+' '+ed+'" --urgency=low --icon=mail-forward')
 
 
 def openRssLink(url):
-    import feedparser
-    import webbrowser
+    # import feedparser
+    # import webbrowser
+    # from furl import furl
 
     rss = feedparser.parse(url)
-    # print ( "Feed Title %s" % rss.feed.title )
+    count = 0
+    # debug_echo( "Feed Title %s" % rss.feed.title )
 
     for entry in rss.entries:
-        # print ( "Title: %s" % entry.title )
-        # print ( "link: %s" % entry.link )
-        webbrowser.open_new_tab(entry.link)
+        debug_echo("Title: %s" % entry.title)
+        debug_echo("link: %s" % entry.link)
 
+        f = furl(entry.link)
+        message_id = f.args['message_id']
+        debug_echo("link: %s" % f.args['message_id'])
+        # webbrowser.open_new_tab(entry.link)
+        webbrowser.open_new_tab(
+            "https://mail.google.com/mail/u/0/h/?&v=c&th=" + message_id)
+
+        count += 1
+        if int(count) > 10:
+            webbrowser.open_new_tab("https://mail.google.com/")
+            print('to many open new mail!')
+            break
     # return entry.link
+    return True
 
 
 def mailRss(url):
-
-    from urllib.request import FancyURLopener
+    # from urllib.request import FancyURLopener
 
     opener = FancyURLopener()
     page = opener.open(url)
@@ -64,55 +171,61 @@ def mailRss(url):
         if int(fullcount) > 1:
             ed += "s"
 
-        # print(' '+fullcount)
-        sendNoti("Gmail", fullcount + ' ' + ed)
+        debug_echo(' ' + fullcount)
+        sendNoti('Gmail', fullcount +' '+ ed)
 
         openRssLink(contents)
 
-    # elif int(fullcount) != 0:
-        # print('error')
-    # else: print('ok')
+    elif int(fullcount) != 0:
+        print('gmail format xml is changed')
+    else:
+        debug_echo('not found new mail')
 
 
 def checkInetConnect():
-    import os
-    import time
-    # import threading
+    # import os
+    # import time
+    ## import threading
 
-    # hostname = "inet"
-    response = os.system("ping -c 3 8.8.8.8 -c1 >/dev/null 2>&1")
+    hostname = "network"
+    reconnect_sec = 10
+    response = os.system('ping -c 3 8.8.8.8 -c1 >/dev/null 2>&1')
 
     if int(response) == 0:
-        # print (hostname, 'is up!')
+        debug_echo(hostname + ' is up!')
         return True
     else:
-        # print (hostname, 'is down!')
-        time.sleep(10)
-        return check_inet()
+        debug_echo(hostname + ' is down!')
+        debug_echo("Trying to reconnect in %s seconds" % reconnect_sec )
+        time.sleep( reconnect_sec )
+        return checkInetConnect()
         # return threading.Timer(2, check_inet).start()
 
 
 def main():
     try:
-        # print ('Working...')
+        module_import(module_list)
+
         print('')
+        debug_echo('Working...')
 
         inet_connect = checkInetConnect()
-
         if inet_connect == True:
-            mailurl = getConfig("/home/art/.local/gmail_settings.cfg")
+            mailurl = getConfig( configFilePath )
             mailRss( mailurl )
 
     except IOError as err:
         # print("I/O error: {0}".format(err))
-        print("mail error")
-    except:
-        print("Fatal error:", sys.exc_info()[0])
+        print('IOError error')
+        raise
+    # except:
+    #     print("Fatal error:", sys.exc_info()[0])
         # raise
     # else:
         # print('else!')
-    # finally:
-        # print('done!')
+    finally:
+        debug_echo('done!')
+        # sys.exit()
 
 if __name__ == '__main__':
     main()
